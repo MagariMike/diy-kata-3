d041988cde2e322d7431aa441cb2956df1d76214
Mike Brewer <michaelandrewbrewer@gmail.com>
2022-12-08 20:01:00 +0000

    finishing fizzBuzz

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,8 +1,14 @@
 const fizzBuzz = (number) => {
+    if (number % 5 === 0 && number % 3 === 0) {
+    return 'fizzBuzz'
+    }
     if (number % 5 === 0) {
     return 'Buzz'
     }
-return 'fizz'
+    if (number % 3 === 0) {
+    return 'fizz'
+    }
+    return number;
 };
 
 module.exports = fizzBuzz;
