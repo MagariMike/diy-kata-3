d920deefc67de840dddfb60457431767e6421459
Mike Brewer <michaelandrewbrewer@gmail.com>
2022-12-10 12:30:25 +0000

    progress on humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,31 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+    let humanYears = number;
+    
+    let catYears = 0;
+    for (let i = 1; i <= number; i++) {
+       if (i === 1) {
+        catYears += 15
+       } else if   (i === 2) {
+        
+      catYears += 9
+       } else {
+        catYears += 4
+       }
+    
+    }
+  
+  let dogYears = 0;
+  for (let i = 1; i <= 1; i++) {
+    if (i === 1) {
+        dogyears += 15
+    } else if (i === 2) {
+        dogYears += 9
+    } else {
+        dogYears += 5
+    }
+  }
+
+  
+};
 
 module.exports = humanCatDogYears;
