1578a627ada71482338dc26038580dcde3dae1cf
Mike Brewer <michaelandrewbrewer@gmail.com>
2022-12-10 11:39:14 +0000

    finished numberToReversedDigits kata

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,8 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (number) => {
+    let stringNum =  String(number);
+    let stringSplit = stringNum.split('');
+    stringSplit.reverse();
+    return stringSplit; 
+};
 
 module.exports = numberToReversedDigits;
