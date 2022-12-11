2d7e7ddf1b8e0a47963f20a72b728fe45d50d656
Mike Brewer <michaelandrewbrewer@gmail.com>
2022-12-11 17:26:00 +0000

    finished Simpsons Kata

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,18 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesArr) => {
+const names = namesArr.map((simpson) => {
+return simpson.name;
+})
+// can just return result of names.reduce eg. return names.reduce...
+let outputString = names.reduce((acc, person, ind) => {
+if (ind <  namesArr.length-1) {
+    return `${acc}, ${person}`
+
+} else { // else not needed as if the first IF statement is not true, it will move on to run the following code\\
+    return `${acc} & ${person}`
+}
+
+})
+return outputString;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,15 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    const names = [{
+
+      name: 'Bart'
+    }, {
+      name: 'Lisa'
+    }, {
+      name: 'Maggie'
+    }]
+    expect(joinNames(names)).toBe('Bart, Lisa & Maggie');
+  });
 });
